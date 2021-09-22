279cebbccd7186367979c8eb6d3240d224b06d5c
Daniel Ferreira <danielteodoro2010@hotmail.com>
2021-09-22 13:44:47 -0300

    v2

---
+++ FILA-H1/Entidade/FilaEstInteiros.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace FILA_H1.Entidade
+{
+    public class GerenciadorFilaAtendimento
+    {
+        private Queue<int> _filaAtendimento;
+        private int _proximoNumeroParaGerar = 1;
+
+        public FilaAtendimento()
+        {
+            _filaAtendimento = new Queue<int>();
+        }
+
+        public int GerarSenha()
+        {
+            _filaAtendimento.Enqueue(_proximoNumeroParaGerar);
+
+            Console.WriteLine("Senha gerada.");
+
+            return _proximoNumeroParaGerar++;
+        }
+
+        public void ChamarProximo()
+        {
+            var proximoNumeroDaFila = _filaAtendimento.Dequeue();
+
+            Console.WriteLine($"Próxima senha: {proximoNumeroDaFila}");
+        }
+
+        public void MostrarFila()
+        {
+            Console.Write("Senhas da Fila: ");
+
+            foreach (var senha in _filaAtendimento)
+            {
+                Console.Write(senha + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public void ReiniciarFila()
+        {
+            _filaAtendimento.Clear();
+        }
+    }
+}
---
+++ FILA-H1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FILA_H1.Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gerenciadorFilaAtendimento = new GerenciadorFilaAtendimento();
+
+            gerenciadorFilaAtendimento.GerarSenha();
+            gerenciadorFilaAtendimento.GerarSenha();
+            gerenciadorFilaAtendimento.GerarSenha();
+
+            gerenciadorFilaAtendimento.MostrarFila();
+
+            gerenciadorFilaAtendimento.ChamarProximo();
+            gerenciadorFilaAtendimento.ChamarProximo();
+
+            gerenciadorFilaAtendimento.MostrarFila();
+
+            gerenciadorFilaAtendimento.GerarSenha();
+
+            gerenciadorFilaAtendimento.ChamarProximo();
+
+            gerenciadorFilaAtendimento.MostrarFila();
+
+            Console.ReadLine();
+        }
+    }
+}
